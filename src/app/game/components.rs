a3311e41e0589562b406e834be826a635b641174
Marcin Flisak <marcinflisak1998@gmail.com>
2021-11-04 23:22:52 +0100

    implement better collision
    Co-authored-by: Nikita Stefaniak <phaux4@gmail.com>

--- src/app/game/components.rs
+++ src/app/game/components.rs
@@ -1,21 +1,26 @@
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub(crate) struct Pos {
     pub(crate) x: i32,
     pub(crate) y: i32,
 }
 
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub(crate) struct Size {
     pub(crate) w: u32,
     pub(crate) h: u32,
 }
 
 /// velocity in 1/10 pixels
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub(crate) struct Vel {
     pub(crate) x: i32,
     pub(crate) y: i32,
 }
 
+#[derive(Debug, Clone, Copy)]
 pub(crate) struct Collision;
 
+#[derive(Debug, Clone, Copy)]
 pub(crate) struct Physics {
     pub(crate) on_floor: bool,
     pub(crate) on_wall: Option<Dir>,
@@ -25,18 +30,21 @@ pub(crate) struct Physics {
     pub(crate) friction: i32,
 }
 
-#[derive(PartialEq)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub(crate) enum Health {
     Alive,
     Dead,
 }
 
+#[derive(Debug, Clone, Copy)]
 pub(crate) struct Player;
 
+#[derive(Debug, Clone, Copy)]
 pub(crate) struct Enemy {
     pub(crate) dir: Dir,
 }
 
+#[derive(Debug, Clone, Copy)]
 pub(crate) enum Dir {
     Left,
     Right,
--- src/app/game/systems/physics.rs
+++ src/app/game/systems/physics.rs
@@ -1,6 +1,10 @@
 use sdl2::rect::Rect;
 
-use crate::app::game::{components::Vel, entity::Entity, Game};
+use crate::app::game::{
+    components::{Pos, Size, Vel},
+    entity::Entity,
+    Game,
+};
 
 pub(crate) fn system(game: &mut Game) {
     let mut collisions = Vec::new();
@@ -33,45 +37,9 @@ pub(crate) fn system(game: &mut Game) {
             ..
         } = entity
         {
-            for other_entity in &game.entities {
-                if let Entity {
-                    pos: Some(other_pos),
-                    size: Some(other_size),
-                    collision: Some(_),
-                    physics: None,
-                    ..
-                } = other_entity
-                {
-                    let mut on_floor = false;
-                    let mut vel = Vel { ..*vel };
-                    let mut did_collide = false;
-
-                    if Rect::new(pos.x, pos.y + vel.y / 10, size.w, size.h).has_intersection(
-                        Rect::new(other_pos.x, other_pos.y, other_size.w, other_size.h),
-                    ) {
-                        if vel.y > 0 {
-                            on_floor = true;
-                        }
-                        vel.y = 0;
-                        did_collide = true;
-                    }
-
-                    if Rect::new(pos.x + vel.x / 10, pos.y + vel.y / 10, size.w, size.h)
-                        .has_intersection(Rect::new(
-                            other_pos.x,
-                            other_pos.y,
-                            other_size.w,
-                            other_size.h,
-                        ))
-                    {
-                        vel.x = 0;
-                        did_collide = true;
-                    }
-
-                    if did_collide {
-                        collisions.push((index, vel, on_floor));
-                    }
-                }
+            let (offset, col_x, col_y) = move_entity(&game.entities, *pos, *size, *vel);
+            if col_x || col_y {
+                collisions.push((index, offset, col_y && vel.y > 0));
             }
         }
     }
@@ -98,4 +66,71 @@ pub(crate) fn system(game: &mut Game) {
             pos.y += vel.y / 10;
         }
     }
+}
+
+/// Tries to move entity with `pos` and `size` by `vel`.
+/// Returns maximum vel before collision with any other entity in `entities`
+/// and whether there was collision in x axis and y axis.
+/// If there was no collision at all returns the given `vel`.
+fn move_entity(entities: &[Entity], pos: Pos, size: Size, vel: Vel) -> (Vel, bool, bool) {
+    let mut vel = vel;
+
+    let mut has_col_x = false;
+    let mut has_col_y = false;
+
+    while vel.x != 0 {
+        if !has_collision(
+            entities,
+            Pos {
+                x: pos.x + vel.x / 10,
+                y: pos.y,
+            },
+            size,
+        ) {
+            break;
+        }
+        has_col_x = true;
+        vel.x -= vel.x.signum();
+    }
+
+    while vel.y != 0 {
+        if !has_collision(
+            entities,
+            Pos {
+                x: pos.x + vel.x / 10,
+                y: pos.y + vel.y / 10,
+            },
+            size,
+        ) {
+            break;
+        }
+        has_col_y = true;
+        vel.y -= vel.y.signum();
+    }
+
+    (vel, has_col_x, has_col_y)
+}
+
+fn has_collision(entities: &[Entity], pos: Pos, size: Size) -> bool {
+    entities
+        .iter()
+        .filter_map(|entity| {
+            if let Entity {
+                pos: Some(other_pos),
+                size: Some(other_size),
+                collision: Some(_),
+                ..
+            } = entity
+            {
+                Some((other_pos, other_size))
+            } else {
+                None
+            }
+        })
+        .any(|(other_pos, other_size)| {
+            Rect::has_intersection(
+                &Rect::new(pos.x, pos.y, size.w, size.h),
+                Rect::new(other_pos.x, other_pos.y, other_size.w, other_size.h),
+            )
+        })
 }
